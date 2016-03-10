697a7d04f180bc9d70c795683b6f1daf29e738f1
Jeroen <jeroen.vandermerwe@gmail.com>
2016-03-10 14:19:38 +0200

    added basic lightcurve plot

--- Irga.R
+++ Irga.R
@@ -1,22 +1,64 @@
-setwd("C:/Users/Dell/Documents/Repositories/Speel/amy")
+##Requires ggplot2
 
-unraw <- function(file){
-        con <- file(file, open = "rt", raw = T)
-        text <- readLines(con, skipNul = T)
-        close(con)
+
+unraw <- function(file, type = "csv", save = FALSE){
         
-        text <- text[11:length(text)]
-        text <- text[-2]
+        #check arguments
+        if(!type %in% c("csv", "xls")){stop("Argument type not recognised, must be either \"csv\" or \"xls\"")}
         
-        text <- text[- grep("Remark", text)]
-        text <- text[- grep("\"", text)]
+        if(type == "csv"){
+                con <- file(file, open = "rt", raw = T)
+                text <- readLines(con, skipNul = T)
+                close(con)
+                
+                text <- text[11:length(text)]
+                text <- text[-2]
+                
+                text <- text[- grep("Remark", text)]
+                text <- text[- grep("\"", text)]
+                
+                text <- strsplit(text, split =",")
+                text <- data.frame(matrix(unlist(text), nrow = length(text), byrow = T), stringsAsFactors = F)
+                text[1,] <- as.character(text[1,])
+                colnames(text) <- text[1, ]
+                text <- text[-1,]
+                text   
+        }
         
-        text <- strsplit(text, split =",")
-        text <- data.frame(matrix(unlist(text), nrow = length(text), byrow = T), stringsAsFactors = F)
-        text[1,] <- as.character(text[1,])
-        colnames(text) <- text[1, ]
-        text <- text[-1,]
-        text
+        #if(type == "xls"){
+        #        data  = read.xlsx(file, sheetIndex = 1, header = TRUE, startRow = 1)
+        #        
+        #}
+        
+        
+        
 }
 
-plotcurve <- function()
+
+
+plotcurve <- function(data, curve = "light"){
+        
+        #check arguments
+        if(!curve %in% c("light", "aci")){stop("curve must be ether \"light\" or \"aci\"")}
+                
+                
+        if(curve == "light"){
+                data$PARi <- as.numeric(data$PARi)
+                data$Photo <- as.numeric(data$Photo)
+                
+                plot <- ggplot(data, aes(data$PARi, data$Photo)) + stat_smooth()
+        }
+        
+        if(curve == "aci"){
+                
+        }
+        
+        plot
+}
+
+
+
+
+
+
+
